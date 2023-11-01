336f03ede49f16f463b62cbaa425e70d367351c2
melanie <lovelystasik@gmail.com>
2023-11-01 18:33:13 +0300

    updt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyB_9LuCSF_nHVEDCSQQtqjEpjf8q07ewTM',
+    authDomain: 'kk-food.firebaseapp.com',
+    projectId: 'kk-food',
+    storageBucket: 'kk-food.appspot.com',
+    messagingSenderId: '1010399022830',
+    appId: '1:1010399022830:web:df22e0b19a8420d13aadff',
+  },
 };
